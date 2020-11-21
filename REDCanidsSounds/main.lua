da3a98464b1f9330333c23019db740bf2ef74adb
Guilherme Tonioli <guilhermestonioli@gmail.com>
2020-11-20 21:26:15 -0300

    Update main.lua

--- REDCanidsSounds/main.lua
+++ REDCanidsSounds/main.lua
@@ -1,4 +1,4 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
-LSM:Register("sound", "Alê corre", [[Interface\Addons\REDCanidsSounds\sound\ale_pica_mula.mp3]])
-LSM:Register("sound", "Alê atenção", [[Interface\Addons\REDCanidsSounds\sound\ale_diminuir_grupo.mp3]])
+LSM:Register("sound", "Alê - Corre", [[Interface\Addons\REDCanidsSounds\sound\ale_pica_mula.mp3]])
+LSM:Register("sound", "Alê - Atenção", [[Interface\Addons\REDCanidsSounds\sound\ale_diminuir_grupo.mp3]])
